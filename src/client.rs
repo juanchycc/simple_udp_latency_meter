44a2c4086bb0abd36b277aee51b76ea045c9abc9
juanchycc <jcaserio@fi.uba.ar>
2023-09-23 14:35:46 -0300

    code

---
+++ src/client.rs
@@ -0,0 +1,18 @@
+use std::net::{UdpSocket, SocketAddr};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let server_ip = "IP_DE_LA_COMPUTADORA_B:12345"; // Cambia esto a la IP de la computadora B
+    let server_addr: SocketAddr = server_ip.parse()?;
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    let message = "Hello, World!"; // El mensaje que quieres enviar
+
+    for _ in 0..10 { // Envía 10 paquetes uno detrás del otro
+        socket.send_to(message.as_bytes(), &server_addr)?;
+
+        // Espera un breve período de tiempo antes de enviar el siguiente paquete
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    Ok(())
+}
---
+++ src/server.rs
@@ -0,0 +1,26 @@
+use std::net::UdpSocket;
+use std::error::Error;
+use std::time::Instant;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let server_ip = "0.0.0.0:12345"; // Cambia esto a la IP de la computadora B
+    let socket = UdpSocket::bind(server_ip)?;
+
+    println!("Server listening on {}", server_ip);
+
+    let mut buf = [0; 1024];
+    let mut start_time: Option<Instant> = None;
+
+    loop {
+        let (_, client_addr) = socket.recv_from(&mut buf)?;
+        if start_time.is_none() {
+            start_time = Some(Instant::now());
+        } else {
+            let end_time = Instant::now();
+            let latency = end_time.duration_since(start_time.unwrap()).as_micros();
+            println!("Latency: {} microseconds from client {}", latency, client_addr);
+            start_time = None;
+        }
+    }
+}
+
